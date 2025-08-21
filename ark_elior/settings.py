6741d41365f0b7f5077ea9926e16aaee6032a2ee
nani <dineshkonala18@gmail.com>
2025-08-22 00:51:52 +0530

    v3.6

--- ark_elior/settings.py
+++ ark_elior/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ark_elior.urls'
@@ -126,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/satic/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home_page', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
