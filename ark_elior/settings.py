17dfdf04afa497e7cb19e7afa4bf7afd18cd8a34
nani <dineshkonala18@gmail.com>
2025-08-22 00:32:18 +0530

    v3.4

--- ark_elior/settings.py
+++ ark_elior/settings.py
@@ -77,12 +77,12 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     "default": dj_database_url.config(
